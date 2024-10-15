918d5b57275f52968c012cbae1a8af33f2bd1487
RoyGuf <roi_8880@gmail.com>
2024-10-15 16:24:59 +0300

    code cleaning

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -9,9 +9,7 @@ import Profile from "@components/Profile";
 const MyProfile = ({params}) => {
     const searchParams = useSearchParams();
     const userName    = searchParams.get('name');
-    const {data: session} = useSession();
     const [posts, setPosts] = useState([]);
-    const router = useRouter();
     useEffect(() => {
         const fetchPosts = async () => {
             const response = await fetch(`/api/users/${params?.id}/posts`);
@@ -22,23 +20,6 @@ const MyProfile = ({params}) => {
         if(params?.id) fetchPosts();
     },[params?.id])
 
-    // const handleEdit = (post) => {
-    //     router.push(`/update-prompt?id=${post._id}`)
-    // }
-    // const handleDelete = async (post) => {
-    //     const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
-    //     if(hasConfirmed) {
-    //         try {
-    //             await fetch(`/api/prompt/${post._id.toString()}`, {
-    //                 method: 'DELETE'
-    //             });
-    //             const filteredPosts = posts.filter((p) => p._id !== post._id);
-    //             setPosts(filteredPosts);
-    //         } catch (error) {
-    //             console.log(error);
-    //         }
-    //     }
-    // }
     return (
         <Profile
             name={userName}
